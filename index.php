73fcfcd808564920c6dc19b1ed5193e28b6485fe
Wojciech Pajtel <wojciech.pajtel@student.pk.edu.pl>
2023-01-29 20:30:49 +0100

    Add routing and php controllers

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'First commit';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('index', 'DefaultController');
+Router::post('login', 'SecurityController');
+
+Router::run($path);
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index()
+    {
+        $this->render('index');
+    }
+
+    public function login()
+    {
+        $this->render('login');
+    }
+
+    public function register()
+    {
+        $this->render('register');
+    }
+
+    public function profile()
+    {
+        $this->render('profile');
+    }
+
+    public function results()
+    {
+        $this->render('results');
+    }
+}
